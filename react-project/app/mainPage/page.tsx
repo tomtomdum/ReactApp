61bf249de2f2712b4c3eb6d33f35680330da5040
Tom <tommy.dumont.pro@gmail.com>
2023-11-26 21:05:06 -0500

    added charts and other components

--- react-project/app/mainPage/page.tsx
+++ react-project/app/mainPage/page.tsx
@@ -3,7 +3,59 @@ import React from 'react'
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
 
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis, AreaChart, CartesianGrid, Area, Tooltip } from "recharts"
 
+
+const data = [
+    {
+        name: "Jan",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Feb",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Mar",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Apr",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "May",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Jun",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Jul",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Aug",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Sep",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Oct",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Nov",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Dec",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+]
 
 const MainPage = () => {
     const { toast } = useToast()
@@ -20,6 +72,48 @@ const MainPage = () => {
             >
                 Show Toast
             </Button>
+
+            <div >salut</div>
+            <ResponsiveContainer width="100%" height={350}>
+                <BarChart data={data}>
+                    <XAxis
+                        dataKey="name"
+                        stroke="#888888"
+                        fontSize={12}
+                        tickLine={false}
+                        axisLine={false}
+                    />
+                    <YAxis
+                        stroke="#888888"
+                        fontSize={12}
+                        tickLine={false}
+                        axisLine={false}
+                        tickFormatter={(value) => `$${value}`}
+                    />
+                    <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+                </BarChart>
+            </ResponsiveContainer>
+
+
+            <AreaChart width={730} height={250} data={data}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            </AreaChart>
         </main>
 
     )
